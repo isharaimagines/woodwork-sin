45fadd2f98f1922b0a00c1960b26f4dc52c3833b
ishararaviget9 <isharasiriwardhanaget99@gmail.com>
2024-04-19 17:43:05 +0530

    initial commit 1next

--- src/i18n.js
+++ src/i18n.js
@@ -16,16 +16,8 @@ i18n
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    fallbackLng: "en",
-    debug: true,
-
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-  });
+  .use(initReactI18next);
+// init i18next
+// for all options read: https://www.i18next.com/overview/configuration-options
 
 export default i18n;
